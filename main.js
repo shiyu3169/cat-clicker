93c0e33f74f1af555a6b3fb496e9db3bfafba279
Shiyu Wang <shiyu3169@gmail.com>
2018-10-25 09:46:22 -0400

    change cat clicker into two cats

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
-document.querySelector("#cat").addEventListener("click", onClick);
+// add event listener for andy and bobby image
+document.querySelector("#andyImg").addEventListener("click", onClickAndy);
+document.querySelector("#bobbyImg").addEventListener("click", onClickBobby);
+// Declare variables for andy and bobby
+let counterAndy = 0;
+let counterBobby = 0;
 
-let counter = 0;
+// Select counter element for andy and bobby
+const counterEleAndy = document.querySelector("#andyCounter");
+const counterEleBobby = document.querySelector("#bobbyCounter");
 
-const counterEle = document.querySelector("#counter");
+counterEleAndy.textContent = counterAndy;
+counterEleBobby.textContent = counterBobby;
 
-counterEle.textContent = counter;
+function onClickAndy() {
+    counterAndy++;
+    counterEleAndy.textContent = counterAndy;
+}
 
-function onClick() {
-    counter++;
-    counterEle.textContent = counter;
+function onClickBobby() {
+    counterBobby++;
+    counterEleBobby.textContent = counterBobby;
 }
